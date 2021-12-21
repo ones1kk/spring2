96472a84b6ab1b52c1f106beb55116a730c700c6
ones1kk <kwsic@naver.com>
2021-12-21 22:44:51 +0900

    ✨ Add : Basic flow

---
+++ src/main/java/hello/advanced/v0/OrderServiceV0.java
@@ -0,0 +1,15 @@
+package hello.advanced.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceV0 {
+
+    private final OrderRepository0 orderRepository;
+
+    public void orderItem(String itemId) {
+        orderRepository.save(itemId);
+    }
+}
