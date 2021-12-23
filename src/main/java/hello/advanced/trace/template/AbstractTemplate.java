38345112151407db11b14e2acad9df3186b8bdae
ones1kk <kwsic@naver.com>
2021-12-23 22:21:04 +0900

    ✨ Add : Template Method Pettern

---
+++ src/main/java/hello/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,29 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public abstract class AbstractTemplate<T> {
+
+    private final LogTrace trace;
+
+    public T execute(String message) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin(message);
+
+            // call logic
+            T result = call();
+
+            trace.end(status);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    protected abstract T call();
+}
