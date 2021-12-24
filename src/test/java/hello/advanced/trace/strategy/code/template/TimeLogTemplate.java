06bb9b1b5fe5fab20047bae7e87827cd7a4c6bad
ones1kk <kwsic@naver.com>
2021-12-24 23:00:41 +0900

    ✅ Add : Template Callback pattern

---
+++ src/test/java/hello/advanced/trace/strategy/code/template/TimeLogTemplate.java
@@ -0,0 +1,20 @@
+package hello.advanced.trace.strategy.code.template;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class TimeLogTemplate {
+
+    public void execute(Callback callback) {
+        long startTime = System.currentTimeMillis();
+
+        // business logic start
+        callback.call();
+        // business logic end
+
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime = {} ", resultTime);
+    }
+
+}
